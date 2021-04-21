9cae12ac0373a6f157bc199469190ee2d5122e6c
Leonardo Martins Do Nascimento <A209555@somoseducacao.com.br>
2021-04-21 14:06:59 -0300

    section allEpisodes no index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -86,7 +86,7 @@ export default function Home( { latesEpisodes, allEpisodes }: HomeProps ) {
                        {allEpisodes.map(episode => {
                            return (
                                <tr key={episode.id}>
-                                   <td>
+                                   <td style={{ width: 72 }}>
                                         <Image 
                                             width={120} 
                                             height={120} 
@@ -97,7 +97,7 @@ export default function Home( { latesEpisodes, allEpisodes }: HomeProps ) {
                                    </td>
                                    <td> <a href="">{episode.title}</a></td>
                                    <td>{episode.members}</td>
-                                   <td>{episode.publishedAt}</td>
+                                   <td style={{ width: 100 }}>{episode.publishedAt}</td>
                                    <td>{episode.durationAsString}</td>
                                    <td>
                                         <button type="button">
